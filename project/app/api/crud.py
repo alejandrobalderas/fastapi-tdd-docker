1a0ff25102cef82889293216a42ce273633d9ee4
Alejandro Balderas <alejandro@balderas.de>
2021-08-17 14:15:12 +0000

    Finish writing tests for summaries

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,3 +1,4 @@
+from typing import Union, List
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
 
@@ -9,3 +10,15 @@ async def post(payload: SummaryPayloadSchema) -> int:
     )
     await summary.save()
     return summary.id
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary[0]
+    return None
+
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
